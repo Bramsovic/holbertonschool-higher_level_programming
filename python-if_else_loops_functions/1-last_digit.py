11aa9e310e8498844db65e1c16237dd0009e1742
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-01-15 15:19:13 +0100

    f-string formatting and improve readability in last digit check

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+lastnumber = abs(number) % 10
+if number < 0:
+    lastnumber = -lastnumber
+if lastnumber > 5:
+    print(f"Last digit of {number} is {lastnumber} and is greater than 5")
+elif lastnumber == 0:
+    print(f"Last digit of {number} is {lastnumber} and is 0")
+else:
+    print(f"Last digit of {number} is {lastnumber}"
+          "and is less than 6 and not 0")
