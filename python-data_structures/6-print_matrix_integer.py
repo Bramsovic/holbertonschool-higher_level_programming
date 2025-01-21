34885093b1805e1c385fdf714587932690e85b02
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-01-21 17:16:19 +0100

    Add function to print a 2D matrix of integers with proper formatting

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for index in range(len(matrix)):
+        for cloumn in range(len(matrix[index])):
+            print("{}".format(matrix[index][cloumn]), end=" ")
+        print()
