792ab8b1f7f19e5c0db59322ca1c2c099293b5e7
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-01-23 19:49:43 +0100

    Fix: Replace f-string with format method for compatibility in print_sorted_dictionary function

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 def print_sorted_dictionary(a_dictionary):
     key = sorted(a_dictionary.keys())
     for index in key:
-        print(f"{index}: {a_dictionary[index]}")
+        print("{}: {}".format(index, a_dictionary[index]))
