c52bde8406de03f2e043c14d27b066087589027c
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-02-04 14:40:41 +0100

    Fix inherits_from function to correctly check subclass instance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,11 +5,4 @@ def inherits_from(obj, a_class):
     Returns True if obj is an instance of a subclass of a_class, else False.
     """
     recup = type(obj)
-    if (
-        isinstance(obj, a_class) and
-        issubclass(recup, a_class) and
-        type(obj) is not a_class
-    ):
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class) and issubclass(recup, a_class) and type(obj) is not a_class
