dfb51e3296841fd45d5f07dcfea70ce21e7b8419
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-02-11 20:22:24 +0100

    Add append_write function to append text to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    """Appends a string to a text file (UTF-8)
+    and returns the number of characters added.
+    Args:
+        filename (str): The path of the file to append to.
+        Defaults to an empty string.
+        text (str): The text to append to the file.
+        Defaults to an empty string.
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        nb_characters_added = f.write(text)
+        return nb_characters_added
