9b4ab7445a096fa58c7e40269ee65f160bbd296e
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-02-12 22:05:41 +0100

    Add reload_from_json method to Student class for updating attributes

---
+++ python-input_output/11-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+    Module class
+"""
+
+
+class Student:
+    """Defines a Student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance.
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary representation of the Student instance.
+        If `attrs` is a list of strings, only the specified attributes
+        are retrieved. Otherwise, all attributes are included.
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+        Returns:
+            dict: The dictionary containing the student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        elif (
+            isinstance(attrs, list)
+            and all(isinstance(element, str) for element in attrs)
+        ):
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
+
+    def reload_from_json(self, json):
+        """Replaces the attributes of the Student
+        instance with values from a dictionary.
+        Args:
+            json (dict): Dictionary where keys are
+            attribute names and values are the new values.
+        The method updates only the attributes
+        that already exist in the instance.
+        """
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
