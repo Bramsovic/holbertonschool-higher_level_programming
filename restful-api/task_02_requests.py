e455320c59e36062eb5cf1099b479709206f1dce
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-02-21 15:00:30 +0100

    Add docstrings and functions to fetch, display, and save posts from JSONPlaceholder API

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,8 +1,24 @@
+#!/usr/bin/python3
+"""
+    Module for fetching and processing posts from an API.
+
+    This module provides functions to:
+    - Retrieve and display post titles from JSONPlaceholder API.
+    - Fetch posts and save them into a CSV file.
+"""
+
 import requests
 import csv
 
 
 def fetch_and_print_posts():
+    """
+    Fetch posts from JSONPlaceholder API and print their titles.
+
+    Sends a GET request to the API and displays
+    the titles of all retrieved posts.
+    Prints an error message if the request fails.
+    """
     url = "https://jsonplaceholder.typicode.com/posts/"
     statu = requests.get(url)
     if statu.status_code == 200:
@@ -15,6 +31,13 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
+    """
+    Fetch posts from JSONPlaceholder API and save them into a CSV file.
+
+    Retrieves posts from the API, extracts 'id', 'title', and 'body',
+    and writes them into a file named 'posts.csv'.
+    Prints an error message if the request fails.
+    """
     url = "https://jsonplaceholder.typicode.com/posts/"
     statu = requests.get(url)
     if statu.status_code == 200:
@@ -26,7 +49,7 @@ def fetch_and_save_posts():
                 "id": post["id"],
                 "title": post["title"],
                 "body": post["body"]
-                })
+            })
         with open("posts.csv", mode="w", encoding="utf-8", newline="") as file:
             fieldnames = ["id", "title", "body"]
             writer = csv.DictWriter(file, fieldnames=fieldnames)
