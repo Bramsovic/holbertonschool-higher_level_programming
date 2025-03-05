64c1d2167b8a423c20678268fe8978a3ff5cb734
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-05 17:10:31 +0100

    Create first_table with columns id andname

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
+-- Create a table named 'first_table' with columns 'id' (INT) and 'name' (VARCHAR) if it does not already exist
 CREATE TABLE IF NOT EXISTS first_table (
     id INT, 
     name VARCHAR(256)
-)
+);
