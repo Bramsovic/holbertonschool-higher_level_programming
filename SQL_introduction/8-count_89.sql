3c757e3d4715895ec154c0d724341925fdba051e
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-06 22:00:42 +0100

    Count the number of rows in first_table where id = 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Count the number of rows in 'first_table' (Note: COUNT(3) will always count all rows, as the argument is ignored)
-SELECT COUNT(3) FROM first_table;
+-- Count the number of rows in 'first_table' where the 'id' is equal to 89
+SELECT count(*) FROM first_table WHERE id = 89;
