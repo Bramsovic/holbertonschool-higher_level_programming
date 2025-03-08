e5c90ffc286867841c4a1b3fb754693adbcac727
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-08 16:32:27 +0100

    Create user_0d_1 and grant all privileges on localhost

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create a new user 'user_0d_1' on the localhost with the specified password
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to 'user_0d_1' on the localhost
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
