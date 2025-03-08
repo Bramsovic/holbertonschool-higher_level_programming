7282a355d1af26379e7f5ccb7707fadb10280a70
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-08 16:05:28 +0100

    Retrieve privileges for user_0d_1 and user_0d_2 on localhost

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Show the privileges granted to 'user_0d_1' on the localhost
+SHOW GRANTS FOR 'user_0d_1@localhost';
+
+-- Show the privileges granted to 'user_0d_2' on the localhost
+SHOW GRANTS FOR 'user_0d_2@localhost';
