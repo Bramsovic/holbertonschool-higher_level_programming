9105c2ce3a0f6cd38e9085b62286ffcbc8814474
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-09 18:23:23 +0100

    Fix SQL query formatting and ORDER BY clause for TV shows and genres

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,5 @@
 -- Use LEFT JOIN to include all TV shows, even those without an associated genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
