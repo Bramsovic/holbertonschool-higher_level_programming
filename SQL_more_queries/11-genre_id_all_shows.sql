159ce336e9788b30444af85b13e6002828797e18
« bramsovic» <haddad.brahim.59000@gmail.com>
2025-03-09 18:12:37 +0100

    Query to list all TV shows with their genre IDs using LEFT JOIN

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Select the title of TV shows along with their corresponding genre ID
+-- Use LEFT JOIN to include all TV shows, even those without an associated genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
